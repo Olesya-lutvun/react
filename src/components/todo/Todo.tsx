d0de81f8591a88c2cfe6854137fb8d648aa984a1
Olesya-lutvun <domchenko.olesya@gmail.com>
2025-01-08 23:00:46 +0200

    lesson 2 task 1

---
+++ src/components/todo/Todo.tsx
@@ -0,0 +1,14 @@
+import { ITodo } from "../../models/ITodo";
+import './todo.css'
+
+interface TodoProps{
+    item:ITodo
+}
+
+export const Todo =({item}: TodoProps) =>{
+    return(
+    <div>
+        <h3 className={item.completed? 'complete' :  'non-complete' }>{item.title}</h3>
+    </div>
+    );
+};
