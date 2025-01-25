0921947af1bb5bf7bc27178d4907870302e3c26e
Olesya-lutvun <domchenko.olesya@gmail.com>
2025-01-25 20:30:47 +0200

    task 7.1 useCallback

--- src/component/UsersComponent.tsx
+++ src/component/UsersComponent.tsx
@@ -1,20 +1,31 @@
-import { useEffect, useState } from "react"; ////Импортирует хук из библиотеки React///
+import {useCallback, useEffect, useState} from "react"; ////Импортирует хук из библиотеки React///
+import {UserComponent} from "./UserComponent";
 
-export const UsersComponent =() =>{ ///Объявляется функциональный компонет///
-    console.log('test') ///будет отбрпжптся в браузере///
+export const UsersComponent = () => { ///Объявляется функциональный компонет///
+    console.log('users')
+
     const [users, setUsers] = useState([]) ///функция, с помощью которой можно обновить состояние///
+
+    const foo = useCallback(() => { /// мемоизирует функцию, чтобы она не
+        // создавалась заново при каждом рендере компонента.///
+
+        console.log('test')
+    }, [])
+
     useEffect(() => {  ///  используется для выполнения  загрузк данных или подписка на события.///
 
-    fetch('http://jsonplaceholder.typecode.com/users')///  использується для получения списка пользователей.///
+        fetch('http://jsonplaceholder.typecode.com/users')///  использується для получения списка пользователей.///
             .then(value => value.json()) ///Преобразует ответ из формата JSON///
-            .then( value => {
+            .then(value => {
                 setUsers(value);
             })
-        return() =>{ ///возвращает  текст "users component".
+        return () => { ///возвращает  текст "users component".
             console.log('unsubscribe')
         }
     }, []);
-    return(
-        <>users component</>
-    )
+    return (
+        <>users component
+            <UserComponent foo={foo}/>
+        </>
+    )///foo передается в UsersComponent, позволяя дочернему компоненту использовать эту функцию ///
 }
